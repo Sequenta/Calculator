bc97c520242a379f1f3f2a4bcfd22cd3de74ee40
Axel <tranquilized.mind@gmail.com>
2013-06-04 14:49:20 +0500

    Интерфейс распознавателя выражений

---
+++ CalculatorSolution/Domain/IRecognizer.cs
@@ -0,0 +1,15 @@
+﻿namespace Domain
+{
+    /// <summary>
+    /// Интерфейс распознавателя выражений
+    /// </summary>
+    public interface IRecognizer
+    {
+        /// <summary>
+        /// Выделяет строку числа из строки выражения 
+        /// </summary>
+        /// <param name="expression">строка выражения</param>
+        /// <returns></returns>
+        string GetFullNumber(string expression);
+    }
+}
