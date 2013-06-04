e60aca265d246829523ad1efd4d46d26e4afa42b
Axel <tranquilized.mind@gmail.com>
2013-06-04 13:56:34 +0500

    Добавлены тесты для InputReader

---
+++ CalculatorSolution/CalculatorSolution.Test/Logic/InputReaderTests.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLogic;
+using Domain;
+using Xunit;
+
+namespace CalculatorSolution.Test.Logic
+{
+    public class InputReaderTests
+    {
+        [Fact]
+        public void Reader_ReturnsCorrectExpression()
+        {
+            IExpressionInputReader reader = new ConsoleInputReader();
+            var result = reader.GetExpression();
+            Assert.Equal("2+3*5",result);
+        }
+    }
+}
