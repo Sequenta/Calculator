85f8b403c8ce3e9ce3074c38a39c415a2913a283
Axel <tranquilized.mind@gmail.com>
2013-06-04 19:02:58 +0500

    Добавлена функция распознавания операций в файлах плагинов

---
+++ CalculatorSolution/BusinessLogic/OperationPluginReader.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using Domain;
+
+namespace BusinessLogic
+{
+    public class OperationPluginReader:IPluginReader
+    {
+        public List<IOperation> ReadPluginsFrom(string directory)
+        {
+            var operations = new List<IOperation>();
+
+            var plugins = GetPluginFiles(directory);
+            foreach (var fileInfo in plugins)
+            {
+                var assembly = Assembly.LoadFrom(fileInfo.FullName);
+                var types = assembly.GetTypes();
+                foreach (var type in types)
+                {
+                    if (typeof(IOperation).IsAssignableFrom(type))
+                    {
+                        var operation = (IOperation)Activator.CreateInstance(type);
+                        operations.Add(operation);
+                    }
+                }
+            }
+            return operations;
+        }
+
+        private IEnumerable<FileInfo> GetPluginFiles(string path)
+        {
+            var directory = new DirectoryInfo(path);
+            if (!directory.Exists)
+            {
+                throw new DirectoryNotFoundException();
+            }
+            return directory.GetFiles("*.dll");
+        }
+    }
+}
