c64055ae110b60666c7907d7cda6186ebddb4855
Axel <tranquilized.mind@gmail.com>
2013-06-04 18:08:36 +0500

    Тест на распознавание операций в dll файлах

---
+++ CalculatorSolution/CalculatorSolution.Test/Plugins/PluginReaderTests.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLogic;
+using Domain;
+using Xunit;
+
+namespace CalculatorSolution.Test.Plugins
+{
+    public class PluginReaderTests
+    {
+        [Fact]
+        public void PluginReader_FindsAllDLLFiles()
+        {
+            IPluginReader pluginReader = new OperationPluginReader();
+            var pluginPath = System.Environment.CurrentDirectory;
+
+            var result = pluginReader.ReadPluginsFrom(pluginPath);
+
+            Assert.Equal(1,result.Count);
+        }
+    }
+}
