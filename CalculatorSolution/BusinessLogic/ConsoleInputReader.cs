b32f29fcbbc79a3188843a98fa621c4cd6b2ca8c
Axel <tranquilized.mind@gmail.com>
2013-06-04 13:59:47 +0500

    Реализация считывателя выражений из консоли

---
+++ CalculatorSolution/BusinessLogic/ConsoleInputReader.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Domain;
+
+namespace BusinessLogic
+{
+    /// <summary>
+    /// Считыватель выражений из консоли
+    /// </summary>
+    public class ConsoleInputReader:IExpressionInputReader
+    {
+        public string GetExpression()
+        {
+            Console.WriteLine("Введите выражение и нажмите Enter");
+            var expression = Console.ReadLine();
+            return expression;
+        }
+    }
+}
