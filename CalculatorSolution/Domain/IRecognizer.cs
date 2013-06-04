fa4c8db664f347e7d4d527bb9c16389908b82f18
Axel <tranquilized.mind@gmail.com>
2013-06-04 15:03:22 +0500

    В интерфейс IRecognizer добавлен метод для распознавания оператора

--- CalculatorSolution/Domain/IRecognizer.cs
+++ CalculatorSolution/Domain/IRecognizer.cs
@@ -11,5 +11,12 @@
         /// <param name="expression">строка выражения</param>
         /// <returns></returns>
         string GetFullNumber(string expression);
+
+        /// <summary>
+        /// Выделяет строку числа из строки выражения
+        /// </summary>
+        /// <param name="expression">строка выражения</param>
+        /// <returns></returns>
+        string GetFullOperation(string expression);
     }
 }
