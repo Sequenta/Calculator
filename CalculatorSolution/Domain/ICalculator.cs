7918fe9c4c2f7745c40e323c164f7a33e9552c03
Axel <tranquilized.mind@gmail.com>
2013-06-05 10:47:58 +0500

    Интерфейс для калькулятора

---
+++ CalculatorSolution/Domain/ICalculator.cs
@@ -0,0 +1,15 @@
+﻿namespace Domain
+{
+    /// <summary>
+    /// Интерфейс для калькулятора
+    /// </summary>
+    public interface ICalculator
+    {
+        /// <summary>
+        /// Возвращает результат вычислениия строки выражения
+        /// </summary>
+        /// <param name="expression">строка выражения</param>
+        /// <returns>результат вычислений</returns>
+        double Calculate(string expression);
+    }
+}
