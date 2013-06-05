47e763a1f3a526e5bddec199f308853932d35449
Axel <tranquilized.mind@gmail.com>
2013-06-05 10:50:57 +0500

    Калькулятор на постфиксной нотации

---
+++ CalculatorSolution/BusinessLogic/PostfixCalculator.cs
@@ -0,0 +1,20 @@
+﻿using Domain;
+
+namespace BusinessLogic
+{
+    /// <summary>
+    /// Калькулятор, основанный на постфиксной нотации
+    /// </summary>
+    public class PostfixCalculator:ICalculator
+    {
+        public double Calculate(string expression)
+        {
+            return 234;
+        }
+
+        public bool IsNumber(string s)
+        {
+            return true;
+        }
+    }
+}
