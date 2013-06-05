c2fda1f15c51dbc3450564789b72a1fedba3209b
Axel <tranquilized.mind@gmail.com>
2013-06-05 12:58:12 +0500

    Тест на правильность рассчетов

--- CalculatorSolution/CalculatorSolution.Test/Calculation/CalculationProcessTests.cs
+++ CalculatorSolution/CalculatorSolution.Test/Calculation/CalculationProcessTests.cs
@@ -1,11 +1,21 @@
-﻿using System.Collections.Generic;
-using BusinessLogic;
+﻿using BusinessLogic;
 using Xunit;
 
 namespace CalculatorSolution.Test.Calculation
 {
     public class CalculationProcessTests
     {
-        
+        [Fact]
+        public void GetOperator_ReturnsCorrectOperator()
+        {
+            var pluginReader = new OperationPluginReader();
+            var operationsList = pluginReader.ReadPluginsFrom(System.Environment.CurrentDirectory + "\\Plugins");
+            var recognizer = new BaseRecognizer(operationsList);
+            var calculator = new PostfixCalculator(recognizer);
+
+            var result = calculator.Calculate("2+2");
+
+            Assert.Equal(4,result);
+        }
     }
 }
