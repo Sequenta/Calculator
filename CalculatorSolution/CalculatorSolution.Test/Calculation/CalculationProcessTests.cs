7e07a04531d06165779555d0334d384c31ae31a7
Axel <tranquilized.mind@gmail.com>
2013-06-05 10:47:18 +0500

    Тест на распознавание числа

---
+++ CalculatorSolution/CalculatorSolution.Test/Calculation/CalculationProcessTests.cs
@@ -0,0 +1,18 @@
+﻿using BusinessLogic;
+using Xunit;
+
+namespace CalculatorSolution.Test.Calculation
+{
+    public class CalculationProcessTests
+    {
+        [Fact]
+        public void Calculator_CanRecognizeDouble()
+        {
+            var calculator = new PostfixCalculator();
+
+            var result = calculator.IsNumber("234");
+
+            Assert.True(result);
+        }
+    }
+}
