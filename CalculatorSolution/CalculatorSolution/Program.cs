a3854172e3b8c1d7e81debe14dcdf889753760d2
Axel <tranquilized.mind@gmail.com>
2013-06-05 17:01:16 +0500

    Рабочий пример калькулятора

--- CalculatorSolution/CalculatorSolution/Program.cs
+++ CalculatorSolution/CalculatorSolution/Program.cs
@@ -1,9 +1,38 @@
-﻿namespace CalculatorSolution
+﻿using System;
+using System.Collections.Generic;
+using BusinessLogic;
+
+namespace CalculatorSolution
 {
     class Program
     {
         static void Main()
         {
+            var pluginReader = new OperationPluginReader();
+            var operations = pluginReader.ReadPluginsFrom(System.Environment.CurrentDirectory + "\\Plugins");
+            var recognizer = new BaseRecognizer(operations);
+            var calculator = new PostfixCalculator(recognizer);
+            ShowAvailableOperations(calculator.GetAvailableOperations());
+            var reader = new ConsoleInputReader();
+            while (true)
+            {
+                var result = calculator.Calculate(reader.GetExpression());
+                Console.WriteLine("Результат: {0}",result);
+                Console.WriteLine("Нажмите Enter, чтобы продолжить или Esc, чтобы выйти");
+                if (Console.ReadKey().Key == ConsoleKey.Escape)
+                {
+                    return;
+                }
+            }
+        }
+
+        private static void ShowAvailableOperations(IEnumerable<string> availableOperations)
+        {
+            Console.WriteLine("Доступны следующие операции:");
+            foreach (var availableOperation in availableOperations)
+            {
+                Console.WriteLine(availableOperation);
+            }
         }
     }
 }
