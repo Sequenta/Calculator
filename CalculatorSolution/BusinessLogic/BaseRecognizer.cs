15c96296413be4dc4d86b8b4c946173c83c57255
Sequenta <tranquilized.mind@gmail.com>
2013-09-10 20:02:16 +0600

    Замена if на ?:

--- CalculatorSolution/BusinessLogic/BaseRecognizer.cs
+++ CalculatorSolution/BusinessLogic/BaseRecognizer.cs
@@ -69,15 +69,8 @@ namespace BusinessLogic
             for (var i = 0; i < expression.Length; i += nextCharacterPosition)
             {
                 var character = expression[i];
-                var recognizedOperand = "";
-                if (char.IsDigit(character))
-                {
-                    recognizedOperand = GetFullNumber(expression.Substring(i, expression.Length - i));
-                }
-                else
-                {
-                    recognizedOperand = GetFullOperation(expression.Substring(i, expression.Length - i));
-                }
+                var recognizedOperand = char.IsDigit(character) ? GetFullNumber(expression.Substring(i, expression.Length - i)) : 
+                                                                  GetFullOperation(expression.Substring(i, expression.Length - i));
                 operands.Add(recognizedOperand);
                 nextCharacterPosition = recognizedOperand.Length;
             }
